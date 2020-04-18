94080d89e0e93ff3306d7de8c95c239b462430df
Daniel Sasser <danielbsasser@gmail.com>
2020-04-18 23:50:14 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,12 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active 
+    @student.save
+  end
+
   private
 
     def set_student
